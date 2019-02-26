5d7143567fbedf2384f8d10a9f8a5a08e26eb96c
wlj <wlj@prodeMacBook-Pro.local>
2019-02-26 18:07:27 +0800

    wang

--- src/components/common/sidebar.js
+++ src/components/common/sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Menu} from 'element-react';
+import { Menu } from 'element-react';
+import { SIDE_BAR_TEXT } from '../meta/sidebarText'
 export default class Sidebar extends React.Component{
   onOpen() {
 
@@ -11,7 +12,18 @@ export default class Sidebar extends React.Component{
   render(){
     return (
         <Menu defaultActive="2" className="el-menu-vertical-demo" onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)}>
-          <Menu.SubMenu index="1" title={<span><i className="el-icon-message"></i>会员管理</span>}>
+        {SIDE_BAR_TEXT.map((item,index)=>{
+          return (
+            <Menu.SubMenu index={item.index} title={<span><i className="el-icon-message"></i>{item.title}</span>} key={index}>
+            {item.subMenu.map((list,key)=>{
+              return (
+                <Menu.Item index={list.index} key={key}>{list.title}</Menu.Item>
+              )
+            })}
+            </Menu.SubMenu>
+          )
+        })}
+          {/* <Menu.SubMenu index="1" title={<span><i className="el-icon-message"></i>会员管理</span>}>
             <Menu.Item index="1-1">会员列表</Menu.Item>
             <Menu.Item index="1-2">注册未申请</Menu.Item>
             <Menu.Item index="1-3">正常还款未借</Menu.Item>
@@ -25,7 +37,7 @@ export default class Sidebar extends React.Component{
             <Menu.Item index="2-5">消耗费用</Menu.Item>
             <Menu.Item index="2-4">进出账</Menu.Item>
             <Menu.Item index="2-5">数据看版</Menu.Item>
-          </Menu.SubMenu>
+          </Menu.SubMenu> */}
         </Menu>
     )
   }
---
+++ src/components/meta/sidebarText.js
@@ -0,0 +1,8 @@
+export const SIDE_BAR_TEXT = [{
+  index: 1,
+  title: '会员管理',
+  subMenu: [{
+    index: '1-1',
+    title: '会员列表'
+  }]
+}]
